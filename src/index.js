be0be7f9936fc72d95b1df1fe0ddd4481fdd3428
Sveta K <test@test.test>
2020-10-05 17:02:54 +0100

    init: start and finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let copyStr = str;
+    const bracketsConfigArr = bracketsConfig.map(pair => pair.join(""));
+
+    let i = 0;
+    while (i < bracketsConfigArr.length) {
+
+        if (copyStr.includes(bracketsConfigArr[i])) {
+            copyStr = copyStr.replace(bracketsConfigArr[i], "");
+            i = 0;
+        } else i++;
+
+    }
+
+    return copyStr.length === 0;
 }
